db5111c8843c3afb8c1dd9d108b5d70d532d8b85
konrad <konrad.klimczak.4@gmail.com>
2016-07-12 20:38:37 +0200

    refactor parsing new layers, replace cloneDeep own method, write new tests

--- spec/leaflet.pin.test.js
+++ spec/leaflet.pin.test.js
@@ -8,25 +8,75 @@ describe('Leaflet.Pin', function () {
 
     describe('#add and parse guide layers', function () {
         var featureGroup;
+        var marker;
+        var poly;
 
         beforeEach(function () {
+            poly = new L.polyline([[50.1, 50,2], [50.1, 50.6]]);
             featureGroup = new L.FeatureGroup();
             featureGroup.addLayer(new L.Marker([50.0, 50.0]));
             featureGroup.addLayer(new L.Marker([50.0, 50.1]));
             featureGroup.addLayer(new L.Marker([50.0, 50.2]));
             map.addGuideLayer(featureGroup);
-            map.addGuideLayer(new L.Marker([50.0, 50.3]));
-        });
-
-        it('correctly added guide layers to global container', function () {
-            expect(map._guides.length).toBe(2);
+            marker = new L.Marker([50.0, 50.3]);
+            map.addLayer(marker);
+            map.addGuideLayer(marker);
         });
 
         it('correctly parse guide layers', function () {
-
-            var pin = new L.Handler.MarkerPin(map);
-            pin.enable(new L.Marker([50.0, 50.3]));
-            expect(pin._guideList.length).toBe(4);
+            expect(map._guideList.length).toEqual(4);
         });
+
+        it('update guide layer for marker', function () {
+            var latlng;
+            map.updateGuideLayer(marker._leaflet_id, L.latLng(50.1, 50.5));
+            for (var i = 0; i < map._guideList.length; i++) {
+                if (map._guideList[i]._leaflet_id == marker._leaflet_id) {
+                    latlng = map._guideList[i].getLatLng();
+                    break;
+                }
+            }
+            expect(latlng.equals(L.latLng(50.1, 50.5))).toBeTruthy();
+        });
+
+        it('update guide layer for poly', function () {
+            var latlngs,
+                newLatLngs = [L.latLng(50.1, 51.2), L.latLng(50.1, 51.6)],
+                poly = new L.polyline([[50.1, 50,2], [50.1, 50.6]]),
+                equals = false;
+
+            map.addLayer(poly);
+            map.addGuideLayer(poly);
+
+            map.updateGuideLayer(poly._leaflet_id, newLatLngs);
+
+            for (var i = 0; i < map._guideList.length; i++) {
+                if (map._guideList[i]._leaflet_id == poly._leaflet_id) {
+                    latlngs = map._guideList[i].getLatLngs();
+                    break;
+                }
+            }
+
+            for (var j = 0; j < latlngs.length; j++) {
+                if (latlngs[j].equals(newLatLngs[j])) {
+                    equals = true;
+                }
+            }
+
+            expect(equals).toBeTruthy();
+        });
+
+        it('add layer to FeatureGroup guide layer', function () {
+            featureGroup.addLayer(poly);
+
+            expect(map._guideList.length).toEqual(5);
+        });
+
+        it('remove layer from FeatureGroup guide layer', function () {
+            featureGroup.addLayer(poly);
+            featureGroup.removeLayer(poly);
+            
+            expect(map._guideList.length).toEqual(4);
+        })
     });
 });
